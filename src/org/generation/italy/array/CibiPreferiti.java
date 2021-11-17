5c1fc0a7aa50ba97cf9be9bedad69c299ca74061
ABozzoli <alessandrobozzoli92@gmail.com>
2021-11-17 15:49:05 +0100

    Aggiunto calcolo lunghezza  classifica

---
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -0,0 +1,15 @@
+package org.generation.italy.array;
+
+public class CibiPreferiti {
+
+	public static void main(String[] args) {
+
+		String[] favFood = {"pizza", "pollo", "pasta", "tonno", "manzo", "cioccolato", "biscotti"};
+		
+		int arrayLength = favFood.length;
+		
+		System.out.println("La classifica contiene " + arrayLength + " elementi.");
+
+	}
+
+}
