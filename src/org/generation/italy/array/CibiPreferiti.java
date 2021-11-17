96010537a7ecb0b0a492c973ce8a98ee80d8cc8d
ABozzoli <alessandrobozzoli92@gmail.com>
2021-11-17 16:15:53 +0100

    Aggiunto bonus

--- src/org/generation/italy/array/CibiPreferiti.java
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -8,11 +8,13 @@ public class CibiPreferiti {
 		
 		int arrayLength = favFood.length;
 		
-		System.out.println("La classifica contiene " + arrayLength + " elementi.");
+		System.out.println("La classifica contiene " + arrayLength + " cibi.");
 
 		System.out.println("Cibo preferito: " + favFood[0] + ".");
 		
 		System.out.println("Cibo preferito ma non troppo: " + favFood[arrayLength - 1] + ".");
+		
+		System.out.println("Cibo a metà della classifica: " + favFood[(arrayLength - 1)/2] + ".");
 		
 	}
 
